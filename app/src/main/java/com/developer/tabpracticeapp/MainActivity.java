f82a6766c8b2910a33e7660c956f42e5a005b6f4
Arslan Ali <marslanali789@gmail.com>
2019-09-08 22:34:35 +0500

    Complete Source Code

---
+++ app/src/main/java/com/developer/tabpracticeapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.developer.tabpracticeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "CHATS");
+        adapter.addFragment(new TwoFragment(), "STATUS");
+        adapter.addFragment(new ThreeFragment(), "CALLS");
+        viewPager.setAdapter(adapter);
+    }
+
+}
